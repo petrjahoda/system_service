bb12e690fb8c9875092eeac01e622a2d3ab8f104
Petr Jahoda <jahoda@zapsi.eu>
2020-02-29 20:09:16 +0100

    ### Removed - MySQL database
    ### Fixed
    - added sleep when creating tables and database is not available
    
    Took 2 minutes

--- create.sh
+++ create.sh
@@ -3,11 +3,12 @@ cd linux
 upx system_service_linux
 cd ..
 cd mac
-upx system_service_mac_darwin
+upx system_service_mac
 cd ..
 cd windows
 upx system_service_windows.exe
 cd ..
+docker pull alpine:latest
 docker rmi -f petrjahoda/system_service:"$1"
 docker build -t petrjahoda/system_service:"$1" .
 docker push petrjahoda/system_service:"$1"
