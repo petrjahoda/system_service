479e6b0844ff3583f994db6a7ff67ea1c727c3c3
petrjahoda <petrjahoda@protonmail.com>
2020-11-24 12:20:27 +0100

    ## [2020.4.2.24] - 2020-11-24
    ### Added
    - menu items for locale table
    
    Took 39 minutes

--- service.go
+++ service.go
@@ -878,7 +878,6 @@ func createLocales(db *gorm.DB) {
 		RuRU: "специальный",
 	}
 	db.Create(&chartsSpecial)
-
 	liveCompany := database.Locale{
 		Name: "navbar-live-company",
 		CsCZ: "společnost",
@@ -907,7 +906,6 @@ func createLocales(db *gorm.DB) {
 		RuRU: "группа",
 	}
 	db.Create(&liveGroup)
-
 	liveWorkplace := database.Locale{
 		Name: "navbar-live-workplace",
 		CsCZ: "pracoviště",
@@ -922,6 +920,484 @@ func createLocales(db *gorm.DB) {
 		RuRU: "рабочее место",
 	}
 	db.Create(&liveWorkplace)
+	statisticCompany := database.Locale{
+		Name: "navbar-statistics-company",
+		CsCZ: "společnost",
+		DeDE: "firma",
+		EnUS: "company",
+		EsES: "empresa",
+		FrFR: "firme",
+		ItIT: "azienda",
+		PlPL: "firma",
+		PtPT: "companhia",
+		SkSK: "spoločnosť",
+		RuRU: "Компания",
+	}
+	db.Create(&statisticCompany)
+	statisticsGroup := database.Locale{
+		Name: "navbar-statistics-group",
+		CsCZ: "skupina",
+		DeDE: "gruppe",
+		EnUS: "group",
+		EsES: "grupo",
+		FrFR: "groupe",
+		ItIT: "gruppo",
+		PlPL: "grupa",
+		PtPT: "grupo",
+		SkSK: "skupina",
+		RuRU: "группа",
+	}
+	db.Create(&statisticsGroup)
+	statisticsWorkplace := database.Locale{
+		Name: "navbar-statistics-workplace",
+		CsCZ: "pracoviště",
+		DeDE: "arbeitsplatz",
+		EnUS: "workplace",
+		EsES: "lugar de trabajo",
+		FrFR: "lieu de travail",
+		ItIT: "posto di lavoro",
+		PlPL: "miejsce pracy",
+		PtPT: "local de trabalho",
+		SkSK: "pracovisko",
+		RuRU: "рабочее место",
+	}
+	db.Create(&statisticsWorkplace)
+
+	statisticsUser := database.Locale{
+		Name: "navbar-statistics-user",
+		CsCZ: "uživatel",
+		DeDE: "nutzer",
+		EnUS: "user",
+		EsES: "usuario",
+		FrFR: "utilisateur",
+		ItIT: "utente",
+		PlPL: "użytkownik",
+		PtPT: "utilizador",
+		SkSK: "užívateľ",
+		RuRU: "пользователь",
+	}
+	db.Create(&statisticsUser)
+
+	statisticsDowntime := database.Locale{
+		Name: "navbar-statistics-downtime",
+		CsCZ: "prostoj",
+		DeDE: "ausfallzeit",
+		EnUS: "downtime",
+		EsES: "inactividad",
+		FrFR: "inactivité",
+		ItIT: "inattività",
+		PlPL: "przestoj",
+		PtPT: "inatividade",
+		SkSK: "prestoj",
+		RuRU: "простой",
+	}
+	db.Create(&statisticsDowntime)
+	statisticsBreakdown := database.Locale{
+		Name: "navbar-statistics-breakdown",
+		CsCZ: "porucha",
+		DeDE: "ausfall",
+		EnUS: "breakdown",
+		EsES: "trastorno",
+		FrFR: "désordre",
+		ItIT: "disturbo",
+		PlPL: "nieład",
+		PtPT: "transtorno",
+		SkSK: "porucha",
+		RuRU: "беспорядок",
+	}
+	db.Create(&statisticsBreakdown)
+	statisticsAlarm := database.Locale{
+		Name: "navbar-statistics-alarm",
+		CsCZ: "alarm",
+		DeDE: "alarm",
+		EnUS: "alarm",
+		EsES: "alarma",
+		FrFR: "alarme",
+		ItIT: "allarme",
+		PlPL: "alarm",
+		PtPT: "alarme",
+		SkSK: "alarm",
+		RuRU: "тревога",
+	}
+	db.Create(&statisticsAlarm)
+
+	trendsCompany := database.Locale{
+		Name: "navbar-trends-company",
+		CsCZ: "společnost",
+		DeDE: "firma",
+		EnUS: "company",
+		EsES: "empresa",
+		FrFR: "firme",
+		ItIT: "azienda",
+		PlPL: "firma",
+		PtPT: "companhia",
+		SkSK: "spoločnosť",
+		RuRU: "Компания",
+	}
+	db.Create(&trendsCompany)
+	trendsGroup := database.Locale{
+		Name: "navbar-trends-group",
+		CsCZ: "skupina",
+		DeDE: "gruppe",
+		EnUS: "group",
+		EsES: "grupo",
+		FrFR: "groupe",
+		ItIT: "gruppo",
+		PlPL: "grupa",
+		PtPT: "grupo",
+		SkSK: "skupina",
+		RuRU: "группа",
+	}
+	db.Create(&trendsGroup)
+	trendsWorkplace := database.Locale{
+		Name: "navbar-trends-workplace",
+		CsCZ: "pracoviště",
+		DeDE: "arbeitsplatz",
+		EnUS: "workplace",
+		EsES: "lugar de trabajo",
+		FrFR: "lieu de travail",
+		ItIT: "posto di lavoro",
+		PlPL: "miejsce pracy",
+		PtPT: "local de trabalho",
+		SkSK: "pracovisko",
+		RuRU: "рабочее место",
+	}
+	db.Create(&trendsWorkplace)
+
+	trendsUser := database.Locale{
+		Name: "navbar-trends-user",
+		CsCZ: "uživatel",
+		DeDE: "nutzer",
+		EnUS: "user",
+		EsES: "usuario",
+		FrFR: "utilisateur",
+		ItIT: "utente",
+		PlPL: "użytkownik",
+		PtPT: "utilizador",
+		SkSK: "užívateľ",
+		RuRU: "пользователь",
+	}
+	db.Create(&trendsUser)
+
+	trendsDowntime := database.Locale{
+		Name: "navbar-trends-downtime",
+		CsCZ: "prostoj",
+		DeDE: "ausfallzeit",
+		EnUS: "downtime",
+		EsES: "inactividad",
+		FrFR: "inactivité",
+		ItIT: "inattività",
+		PlPL: "przestoj",
+		PtPT: "inatividade",
+		SkSK: "prestoj",
+		RuRU: "простой",
+	}
+	db.Create(&trendsDowntime)
+	trendsBreakdown := database.Locale{
+		Name: "navbar-trends-breakdown",
+		CsCZ: "porucha",
+		DeDE: "ausfall",
+		EnUS: "breakdown",
+		EsES: "trastorno",
+		FrFR: "désordre",
+		ItIT: "disturbo",
+		PlPL: "nieład",
+		PtPT: "transtorno",
+		SkSK: "porucha",
+		RuRU: "беспорядок",
+	}
+	db.Create(&trendsBreakdown)
+	trendsAlarm := database.Locale{
+		Name: "navbar-trends-alarm",
+		CsCZ: "alarm",
+		DeDE: "alarm",
+		EnUS: "alarm",
+		EsES: "alarma",
+		FrFR: "alarme",
+		ItIT: "allarme",
+		PlPL: "alarm",
+		PtPT: "alarme",
+		SkSK: "alarm",
+		RuRU: "тревога",
+	}
+	db.Create(&trendsAlarm)
+
+	trendsTop := database.Locale{
+		Name: "navbar-trends-top10",
+		CsCZ: "10 nejlepších",
+		DeDE: "beste 10",
+		EnUS: "top 10",
+		EsES: "10 mejores",
+		FrFR: "meilleur 10",
+		ItIT: "migliori",
+		PlPL: "10 najlepszych",
+		PtPT: "melhores 10",
+		SkSK: "10 najlepších",
+		RuRU: "10 лучших",
+	}
+	db.Create(&trendsTop)
+
+	trendsWorst := database.Locale{
+		Name: "navbar-trends-worst10",
+		CsCZ: "10 nejhorších",
+		DeDE: "schlimmste 10",
+		EnUS: "worst 10",
+		EsES: "10 peores",
+		FrFR: "pire 10",
+		ItIT: "peggiore 10",
+		PlPL: "10 najgorszych",
+		PtPT: "piores 10",
+		SkSK: "10 najhorších",
+		RuRU: "10 худшие",
+	}
+	db.Create(&trendsWorst)
+
+	dataCompany := database.Locale{
+		Name: "navbar-data-company",
+		CsCZ: "společnost",
+		DeDE: "firma",
+		EnUS: "company",
+		EsES: "empresa",
+		FrFR: "firme",
+		ItIT: "azienda",
+		PlPL: "firma",
+		PtPT: "companhia",
+		SkSK: "spoločnosť",
+		RuRU: "Компания",
+	}
+	db.Create(&dataCompany)
+	dataGroup := database.Locale{
+		Name: "navbar-data-group",
+		CsCZ: "skupina",
+		DeDE: "gruppe",
+		EnUS: "group",
+		EsES: "grupo",
+		FrFR: "groupe",
+		ItIT: "gruppo",
+		PlPL: "grupa",
+		PtPT: "grupo",
+		SkSK: "skupina",
+		RuRU: "группа",
+	}
+	db.Create(&dataGroup)
+	dataWorkplace := database.Locale{
+		Name: "navbar-data-workplace",
+		CsCZ: "pracoviště",
+		DeDE: "arbeitsplatz",
+		EnUS: "workplace",
+		EsES: "lugar de trabajo",
+		FrFR: "lieu de travail",
+		ItIT: "posto di lavoro",
+		PlPL: "miejsce pracy",
+		PtPT: "local de trabalho",
+		SkSK: "pracovisko",
+		RuRU: "рабочее место",
+	}
+	db.Create(&dataWorkplace)
+
+	dataUser := database.Locale{
+		Name: "navbar-data-user",
+		CsCZ: "uživatel",
+		DeDE: "nutzer",
+		EnUS: "user",
+		EsES: "usuario",
+		FrFR: "utilisateur",
+		ItIT: "utente",
+		PlPL: "użytkownik",
+		PtPT: "utilizador",
+		SkSK: "užívateľ",
+		RuRU: "пользователь",
+	}
+	db.Create(&dataUser)
+
+	dataDowntime := database.Locale{
+		Name: "navbar-data-downtime",
+		CsCZ: "prostoj",
+		DeDE: "ausfallzeit",
+		EnUS: "downtime",
+		EsES: "inactividad",
+		FrFR: "inactivité",
+		ItIT: "inattività",
+		PlPL: "przestoj",
+		PtPT: "inatividade",
+		SkSK: "prestoj",
+		RuRU: "простой",
+	}
+	db.Create(&dataDowntime)
+	dataBreakdown := database.Locale{
+		Name: "navbar-data-breakdown",
+		CsCZ: "porucha",
+		DeDE: "ausfall",
+		EnUS: "breakdown",
+		EsES: "trastorno",
+		FrFR: "désordre",
+		ItIT: "disturbo",
+		PlPL: "nieład",
+		PtPT: "transtorno",
+		SkSK: "porucha",
+		RuRU: "беспорядок",
+	}
+	db.Create(&dataBreakdown)
+	dataAlarm := database.Locale{
+		Name: "navbar-data-alarm",
+		CsCZ: "alarm",
+		DeDE: "alarm",
+		EnUS: "alarm",
+		EsES: "alarma",
+		FrFR: "alarme",
+		ItIT: "allarme",
+		PlPL: "alarm",
+		PtPT: "alarme",
+		SkSK: "alarm",
+		RuRU: "тревога",
+	}
+	db.Create(&dataAlarm)
+
+	dataOrder := database.Locale{
+		Name: "navbar-data-order",
+		CsCZ: "zakázka",
+		DeDE: "bestellung",
+		EnUS: "order",
+		EsES: "orden",
+		FrFR: "ordre",
+		ItIT: "ordinazione",
+		PlPL: "zamówienie",
+		PtPT: "ordem",
+		SkSK: "zakázka",
+		RuRU: "заказ",
+	}
+	db.Create(&dataOrder)
+
+	dataProduct := database.Locale{
+		Name: "navbar-data-product",
+		CsCZ: "produkt",
+		DeDE: "produkt",
+		EnUS: "product",
+		EsES: "producto",
+		FrFR: "produit",
+		ItIT: "prodotto",
+		PlPL: "produkt",
+		PtPT: "produtos",
+		SkSK: "produkt",
+		RuRU: "товар",
+	}
+	db.Create(&dataProduct)
+
+	settingsCompany := database.Locale{
+		Name: "navbar-settings-company",
+		CsCZ: "společnost",
+		DeDE: "firma",
+		EnUS: "company",
+		EsES: "empresa",
+		FrFR: "firme",
+		ItIT: "azienda",
+		PlPL: "firma",
+		PtPT: "companhia",
+		SkSK: "spoločnosť",
+		RuRU: "Компания",
+	}
+	db.Create(&settingsCompany)
+	settingsGroup := database.Locale{
+		Name: "navbar-settings-group",
+		CsCZ: "skupina",
+		DeDE: "gruppe",
+		EnUS: "group",
+		EsES: "grupo",
+		FrFR: "groupe",
+		ItIT: "gruppo",
+		PlPL: "grupa",
+		PtPT: "grupo",
+		SkSK: "skupina",
+		RuRU: "группа",
+	}
+	db.Create(&settingsGroup)
+	settingsWorkplace := database.Locale{
+		Name: "navbar-settings-workplace",
+		CsCZ: "pracoviště",
+		DeDE: "arbeitsplatz",
+		EnUS: "workplace",
+		EsES: "lugar de trabajo",
+		FrFR: "lieu de travail",
+		ItIT: "posto di lavoro",
+		PlPL: "miejsce pracy",
+		PtPT: "local de trabalho",
+		SkSK: "pracovisko",
+		RuRU: "рабочее место",
+	}
+	db.Create(&settingsWorkplace)
+
+	settingsUser := database.Locale{
+		Name: "navbar-settings-user",
+		CsCZ: "uživatel",
+		DeDE: "nutzer",
+		EnUS: "user",
+		EsES: "usuario",
+		FrFR: "utilisateur",
+		ItIT: "utente",
+		PlPL: "użytkownik",
+		PtPT: "utilizador",
+		SkSK: "užívateľ",
+		RuRU: "пользователь",
+	}
+	db.Create(&settingsUser)
+
+	settingsDowntime := database.Locale{
+		Name: "navbar-settings-downtime",
+		CsCZ: "prostoj",
+		DeDE: "ausfallzeit",
+		EnUS: "downtime",
+		EsES: "inactividad",
+		FrFR: "inactivité",
+		ItIT: "inattività",
+		PlPL: "przestoj",
+		PtPT: "inatividade",
+		SkSK: "prestoj",
+		RuRU: "простой",
+	}
+	db.Create(&settingsDowntime)
+	settingsBreakdown := database.Locale{
+		Name: "navbar-settings-breakdown",
+		CsCZ: "porucha",
+		DeDE: "ausfall",
+		EnUS: "breakdown",
+		EsES: "trastorno",
+		FrFR: "désordre",
+		ItIT: "disturbo",
+		PlPL: "nieład",
+		PtPT: "transtorno",
+		SkSK: "porucha",
+		RuRU: "беспорядок",
+	}
+	db.Create(&settingsBreakdown)
+	settingsAlarm := database.Locale{
+		Name: "navbar-settings-alarm",
+		CsCZ: "alarm",
+		DeDE: "alarm",
+		EnUS: "alarm",
+		EsES: "alarma",
+		FrFR: "alarme",
+		ItIT: "allarme",
+		PlPL: "alarm",
+		PtPT: "alarme",
+		SkSK: "alarm",
+		RuRU: "тревога",
+	}
+	db.Create(&settingsAlarm)
+
+	settingsDevice := database.Locale{
+		Name: "navbar-settings-device",
+		CsCZ: "zařízení",
+		DeDE: "gerät",
+		EnUS: "device",
+		EsES: "aparato",
+		FrFR: "l'appareil",
+		ItIT: "dispositivo",
+		PlPL: "urządzenie",
+		PtPT: "dispositivo",
+		SkSK: "zariadenie",
+		RuRU: "устройство",
+	}
+	db.Create(&settingsDevice)
 }
 
 func checkDatabaseOnly() bool {
